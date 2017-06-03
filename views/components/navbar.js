6241dcf0dc3edf75db0b8e9c9dd860b6aaf12ad1
Elijah <andersene909@gmail.com>
2017-06-03 11:44:28 -0700

    Dashboard requires login to view; Headers hide/show;

---
+++ views/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Header extends React.Component {
+	render(){
+		return <h1>This currently does nothing at the moment. Will need to convert the current JS to React.JS</h1>;
+	}
+}
+
+ReactDOM.render(
+	test,
+	document.getElementById('root')
+);
